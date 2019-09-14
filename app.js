e5f1cc5ca05584b256ce9881161d6a3ba7d80c90
Chris Whitney <drifterz28@users.noreply.github.com>
2019-09-13 21:01:22 -0700

    Running server

---
+++ app.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const express = require('express');
+const Bundler = require('parcel-bundler');
+
+const env = process.env;
+const app = express();
+const bundler = new Bundler('./src/index.html');
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.use(express.static('dist'));
+
+app.get('/test', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use(bundler.middleware());
+
+app.listen(port, () => {
+  console.log(`Node app is running on http://0.0.0.0:${port}`);
+});
