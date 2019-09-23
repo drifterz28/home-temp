60571c76635b1937941631443467a2a425063ea3
Chris Whitney <drifterz28@users.noreply.github.com>
2019-09-23 11:27:08 -0700

    Update connect

--- src/lib/connect.js
+++ src/lib/connect.js
@@ -1,4 +1,18 @@
 const { Pool } = require('pg');
+
+let connectionString = {
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  host: process.env.HOST
+};
+
+if (process.env.NODE_ENV === 'production') {
+  connectionString = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  };
+};
+
 const pool = new Pool();
 module.exports = {
   query: (text, params = []) => pool.query(text, params),
