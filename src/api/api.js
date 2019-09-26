bff436fb19544dfa967698b4f14363cb4429d6aa
Chris Whitney <drifterz28@users.noreply.github.com>
2019-09-25 21:57:14 -0700

    Update insert

--- src/api/api.js
+++ src/api/api.js
@@ -19,7 +19,7 @@ const getDateRange = (range) => {
 };
 
 async function setRoomTemp(query) {
-  db.query("INSERT INTO temps(ip, temp, hum) VALUES ($1, $2, $3)", [ query.ip, query.temp, query.hum ]);
+  db.query("INSERT INTO temps(ip, temp, hum, timestamp) VALUES ($1, $2, $3, current_timestamp)", [ query.ip, query.temp, query.hum ]);
 };
 
 async function getRoomTemps({room, range = 'day'}) {
