6cb31ea273422fb59a8072ae2a7e04fb04bd506d
Chris Whitney <drifterz28@users.noreply.github.com>
2019-10-01 21:29:43 -0700

    query

--- arduino/home-temp/home-temp.ino
+++ arduino/home-temp/home-temp.ino
@@ -1,32 +1,73 @@
-#include <math.h>
+#include "DHT.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-#define thermister 2
+// change these values
+const char* ssid = "empire-2.4";
+const char* pass = "5038038883";
+String url = "http://home.drifterz28.com/api/temp";
+int main_delay = 30; // delay in min for the loop
+bool isFahrenheit = true;
+// no change past here
 
-//User Inputs (Constant Values)
-const int R2 = 1000.0; //Ohms
-const int Ro = 10000.0; //Ohms   Thermistor resistance @ 25 degrees C or 298.2K
-const int To = 298.2; //Kelvin
-const int B = 3950.0; //Kelvin   B value for your Thermistor
-const int Vin = 3.4; //Volts     Input Voltage
-
+#define DHTPIN 2     // sensor pin connected to
+#define DHTTYPE DHT22   // DHT 22 sensor
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println("WiFi temp link");
+  Serial.println();
+  delay(4000);
+  dht.begin();
+  WiFi.begin(ssid, pass);
+  Serial.print("Wait for WiFi... ");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  WiFi.softAPdisconnect();
+  Serial.println(WiFi.localIP());
+  delay(500);
 }
 
-int getTemp() {
-  int AnalogSensor = analogRead(thermister);
-  float Vo = AnalogSensor * (Vin / 1023.0);
-  //Find R1
-  float R1 = (R2 / Vo) * Vin - R2;
-  //Calculate Temperature using the Thermistor equation
-  float Tk = pow(((1.0 / To) + (1.0 / B) * log(R1 / Ro)), -1); //Kelvin
-  float Tc = Tk - 273.2; //Celcius conversion
-  float Tf = (9.0 / 5.1) * Tc + 32; //Celcius to Fahrenheit converstion
-  return round(Tf);
+float getHumidity() {
+  float humidity = dht.readHumidity();
+  if (isnan(humidity)) {
+    Serial.println("bad humidity, re-run");
+    delay(5000);
+    humidity = getHumidity();
+  }
+  Serial.print("Humidity: ");
+  Serial.println(humidity);
+  return humidity;
+}
+
+float getTemp() {
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float temp_f = dht.readTemperature(isFahrenheit);
+  if (isnan(temp_f)) {
+    Serial.println("bad temp, rerun");
+    delay(5000);
+    temp_f = getTemp();
+  }
+  Serial.print("Temperature: ");
+  Serial.println(temp_f);
+  return temp_f;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (WiFi.status() == WL_CONNECTED) {
+    String ipAddress = WiFi.localIP().toString();
+    String fullUrl = url + "?ip=" + ipAddress + "&temp=" + String(getTemp()) + "&hum=" + String(getHumidity());
+    Serial.print("Send to server: ");
+    Serial.println(fullUrl);
+    HTTPClient http;
+    http.begin(fullUrl);
+    int httpCode = http.GET();
+    if (httpCode != -1) {
+      Serial.println("data sent to server");
+      delay(1000 * 60 * main_delay);
+    } else {
+      Serial.println("fail");
+      delay(50000); // run in 5 min on fail
+    }
+  }
 }
